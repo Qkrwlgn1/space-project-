e2f9b1dbe9b3776be858b3b2612c6716d15f0237
orc-yy <whleewhlee1004@naver.com>
2025-08-10 21:36:16 +0900

    기본적인 메뉴 완성

--- Assets/Scripts/UI/SettingMenu.cs
+++ Assets/Scripts/UI/SettingMenu.cs
@@ -29,30 +29,73 @@ public class SettingMenu : MonoBehaviour
         switch (name)
         {
             case "Desc":
-                _menu[0].SetActive(true);
+                if (GameManager.instance.isGameStarted && _menu[2].activeInHierarchy)
+                {
+                    GameManager.instance.Stop();
+                    _menu[2].SetActive(false);
+                    _menu[0].SetActive(true);
+                }
+                else
+                {
+                    _menu[0].SetActive(true);
+                }
                 break;
             case "Setting":
-                _menu[1].SetActive(true);
+                if (GameManager.instance.isGameStarted && _menu[2].activeInHierarchy)
+                {
+                    GameManager.instance.Stop();
+                    _menu[2].SetActive(false);
+                    _menu[1].SetActive(true);
+                }
+                else
+                {
+                    _menu[1].SetActive(true);
+                }
                 break;
             case "EscMenu":
-                _menu[2].SetActive(true);
+                if (GameManager.instance.isGameStarted)
+                {
+                    GameManager.instance.Stop();
+                    _menu[2].SetActive(true);
+                }
+                break;
+            case "ExitGame":
+                Application.Quit();
                 break;
         }
+
+        
     }
     public void DisActiveMenu(string name)
     {
         switch (name)
         {
             case "Desc":
-                _menu[0].SetActive(false);
+                if (GameManager.instance.isGameStarted && _menu[0].activeInHierarchy)
+                {
+                    _menu[0].SetActive(false);
+                    _menu[2].SetActive(true);
+                }
+                else
+                {
+                    _menu[0].SetActive(false);
+                }
                 break;
             case "Setting":
-                _menu[1].SetActive(false);
+                if (GameManager.instance.isGameStarted && _menu[1].activeInHierarchy)
+                {
+                    _menu[1].SetActive(false);
+                    _menu[2].SetActive(true);
+                }
+                else
+                {
+                    _menu[1].SetActive(false);
+                }
                 break;
             case "EscMenu":
                 _menu[2].SetActive(false);
+                GameManager.instance.Resume();
                 break;
         }
     }
-
 }
